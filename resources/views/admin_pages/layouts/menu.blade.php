8e35cb7b27e7ecc9496975bb0efca5802ccd78bc
sidiki123 <sidikiouedraogo2000@gmail.com>
2020-08-07 16:00:15 +0000

    amelioration front-end

--- resources/views/admin_pages/layouts/menu.blade.php
+++ resources/views/admin_pages/layouts/menu.blade.php
@@ -117,7 +117,37 @@
                         </li>
                         {{-- <li> <a class="waves-effect waves-dark" href="" aria-expanded="false"><i class="mdi mdi-gmail"></i><span class="hide-menu">E-mail</span></a>
                         </li> --}}
-                       
+                        <ul class="navbar-nav ml-auto">
+                                <!-- Authentication Links -->
+                                @guest
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    </li>
+                                    @if (Route::has('register'))
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                        </li>
+                                    @endif
+                                @else
+                                    <li class="nav-item dropdown">
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                            {{ Auth::user()->name }} <span class="caret"></span>
+                                        </a>
+        
+                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                               onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
+                                                {{ __('Logout') }}
+                                            </a>
+        
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                @csrf
+                                            </form>
+                                        </div>
+                                    </li>
+                                @endguest
+                            </ul>
                     </ul>
                     
                 </nav>
@@ -127,6 +157,8 @@
             <!-- Bottom points-->
             <div class="sidebar-footer">
                 
-                <!-- item--><a href="" class="link" data-toggle="tooltip" title="Logout"><i class="mdi mdi-power"></i></a> </div>
+                    
+            </div>
+                
             <!-- End Bottom points-->
         </aside>
